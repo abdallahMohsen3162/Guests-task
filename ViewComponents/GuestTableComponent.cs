574a9739f595bd66c09eccb76a194bb0e444f180
Abdallah Mohsen <93709847+abdallahMohsen3162@users.noreply.github.com>
2024-08-06 16:44:34 +0300

    Add files via upload

---
+++ ViewComponents/GuestTableComponent.cs
@@ -0,0 +1,25 @@
+﻿using hendi.Data;
+using hendi.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace hendi.ViewComponents
+{
+    public class GuestTableViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _dbcontext;
+        public GuestTableViewComponent(ApplicationDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+        public IViewComponentResult Invoke()
+        {
+
+            var model = new GuestViewModel
+            {
+                Guests = _dbcontext.Guests.ToList()
+            };
+            return View(model);
+        }
+    }
+}
